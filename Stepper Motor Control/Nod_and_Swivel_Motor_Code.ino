268324481b5132e102761ef318f7c65fc8299a62
Sohun8 <73727606+Sohun8@users.noreply.github.com>
2022-05-20 17:44:06 -0700

    Add files via upload
    Establishes Connection with Pi (disable comment in setup function). Also accounts for limit switches.

--- Stepper Motor Control/Nod_and_Swivel_Motor_Code.ino
+++ Stepper Motor Control/Nod_and_Swivel_Motor_Code.ino
@@ -1,4 +1,5 @@
 #include "SparkFun_ProDriver_TC78H670FTG_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_ProDriver
+#include "ezButton.h"
 PRODRIVER nodMotor; // Controls up and down movement
 PRODRIVER swivelMotor;  // Controls swiveling movement
 String command = "nothing";
@@ -6,9 +7,13 @@ String swivelCommand = "nothing";
 String nodCommand = "nothing";
 String stopCommand = "nothing";
 
+int turnSwitch = 49;
+int nodSwitch = 47;
+
+void readSerialPort();
+
 void setup() {
-   Serial.begin(115200);
-   Serial.println("Waiting for command");
+   Serial.begin(9600);
 
   // Initialize Driver Pins on Arduino Mega
   nodMotor.settings.standbyPin = 13;
@@ -26,19 +31,50 @@ void setup() {
   swivelMotor.settings.mode2Pin = A10;
   swivelMotor.settings.mode3Pin = A9;
   swivelMotor.settings.errorPin = A14;
-
+  // uncomment this when the raspberry pi is connected
+  // while (!establish_connection()) {}
+  
   // Set motor step resolution for both motors
   nodMotor.settings.stepResolutionMode = PRODRIVER_STEP_RESOLUTION_FIXED_1_16; // 1/16 step
   swivelMotor.settings.stepResolutionMode = PRODRIVER_STEP_RESOLUTION_FIXED_1_16; 
+  nodMotor.begin();
+  swivelMotor.begin();
 
-   nodMotor.begin();
-   swivelMotor.begin();
+  pinMode(turnSwitch, INPUT_PULLUP);
+
+  // Home Turn Motor
+  Serial.println("Homing Turn Motor");
+  while(digitalRead(turnSwitch)){
+    swivelMotor.step(1,0);
+  }
+  while(!digitalRead(turnSwitch)){
+    swivelMotor.step(1,1);
+  }
+  
+  int startTime = millis();
+  // Home Nod Motor
+  Serial.println("Homing Nod Motor");
+  while(digitalRead(nodSwitch)){
+     if(millis() - startTime < 10000){
+      nodMotor.step(1,1);
+    }
+    else{
+      nodMotor.step(1,0);
+    }
+  }
+  while(!digitalRead(nodSwitch)){
+    nodMotor.step(1,1);
+  }
+
+
+
+   Serial.println("Waiting for command");
 }
 
 void loop() {
-  if (Serial.available() > 0){
-    command = Serial.readString();
-    command.remove(command.length()-1);
+   readSerialPort();
+ 
+    //command.remove(command.length()-1);
     if (command.startsWith("Turn") > 0){
       swivelCommand = command;
       Serial.println(swivelCommand);
@@ -49,28 +85,27 @@ void loop() {
     else if (command.startsWith("Stop") > 0){
       stopCommand = command;
     }
-  }
   
 
 
 
   if (swivelCommand == "Turn Right"){
-    swivelMotor.step(50, 1);
+    swivelMotor.step(10, 1);
     Serial.println("Turning Right");
   }
   
-  if (swivelCommand == "Turn Left"){
-    swivelMotor.step(50, 0);
+  else if (swivelCommand == "Turn Left"){
+    swivelMotor.step(10, 0);
     Serial.println("Turning Left");
   }
 
   if (nodCommand == "Nod Up"){
-    nodMotor.step(50, 0);
+    nodMotor.step(10, 0);
     Serial.println("Nodding Up");
   }
 
-  if (nodCommand == "Nod Down"){
-    nodMotor.step(50, 1);
+  else if (nodCommand == "Nod Down"){
+    nodMotor.step(10, 1);
     Serial.println("Nodding Down");
   }
 
@@ -79,9 +114,45 @@ void loop() {
     Serial.println("Stopped Turning");
     stopCommand = "nothing";
   }
-  if (stopCommand == "Stop Nodding"){
+  
+  else if (stopCommand == "Stop Nodding"){
     nodCommand = "nothing";
     Serial.println("Stopped Nodding");
     stopCommand = "nothing";
   }
+}
+
+void readSerialPort() {
+  command = "";
+  if (Serial.available()) {
+    delay(10);
+    while (Serial.available() > 0) {
+      command += (char)Serial.read();
+    }
+    Serial.flush();
+  }
+}
+
+//This function estbalishes a connection with the raspberry pi (currently it is never called because the raspberry pi is not being used)
+boolean establish_connection() {
+    while (!(Serial.available() > 0)) {} // hangs until a msg is received
+    char rcv = Serial.read();
+    if (rcv == 'i') {
+        Serial.println("motor");
+    }
+
+    int start = millis();
+    while (!(Serial.available() > 0)) {
+        if (millis() - start > 1000) {
+            return false;
+        }
+    }
+    rcv = Serial.read();
+    if (rcv == 'd') {
+        // Pi has acknowledged this arduino
+        return true;
+    }
+    else {
+        return false;
+    }
 }
